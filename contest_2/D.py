b233e4887297ca90d3e624907b23be41b9b6e24e
cr <cswd@yandex.ru>
2024-02-29 00:16:59 +0700

    .

--- contest_2/D.py
+++ contest_2/D.py
@@ -20,15 +20,9 @@ for i in lst:
             streak = 1
         if len(stack) and i == stack[-1]:
             streak += 1
+        else:
+            streak = 1
     else:
         streak = 1
     stack.append(i)
 print(destroyed)
-'''
-10
-3 3 2 1 1 1 2 2 3 3
-34
-0 0 0 1 2 3 4 5 6 7 8 9 0 0 9 9 8 8 7 7 6 6 5 5 4 4 3 3 2 2 1 1 0 0
-5
-0 1 1 1 1 0 0 2
-'''
--- contest_2/F.py
+++ contest_2/F.py
@@ -1,30 +1,24 @@
+# не заходит на питоне, только на c++ и то надо с бубном потанцевать
 from collections import deque
 
 n = int(input())
 d = deque()
+database = [0] * 2 * 10 ** 5
+out = 0
 for i in range(n):
     query = input()
     _type = int(query[0])
     if _type == 1:
         _id = int(query.split()[1])
+        database[_id] = len(d) + out
         d.append(_id)
     elif _type == 2:
         d.popleft()
+        out += 1
     elif _type == 3:
         d.pop()
     elif _type == 4:
         _id = int(query.split()[1])
-        print(d.index(_id))
+        print(database[_id] - out)
     elif _type == 5:
         print(d[0])
-'''
-7
-1 1
-5
-1 3
-3
-2
-1 2
-4 2
-
-'''
