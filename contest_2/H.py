6d04eaa5ef34159015d4506430b525278d2b8911
cr <cswd@yandex.ru>
2024-03-09 19:41:57 +0700

    contest_2 H added

--- contest_2/H.py
+++ contest_2/H.py
@@ -1,47 +1,39 @@
-# не решена
-def min_pair2():
-    idx = [[-1, -1]]
-    for i in range(1, n):
-        j = i - 1
-        if lst[j] < lst[i]:
-            idx.append([j, i])
-        else:
-            for j in range(len(idx) - 1, -1, -1):
-                if lst[idx[j][0]] < lst[i]:
-                    idx.append([idx[j][0], i])
-                    break
-            else:
-                idx.append([-1, -1])
-    return idx
-
-
-def choose_best_sum(min_ind, elem_ind):
-    if elem_ind == -1:
-        return -1
-    sums = set()
-    sums.add((prefix_sums[elem_ind] - prefix_sums[min_ind - 1]) * lst[min_ind])
-    sums.add((prefix_sums[elem_ind] - prefix_sums[min_ind]) * lst[elem_ind])
-    sums.add((prefix_sums[elem_ind - 1] - prefix_sums[min_ind]) * lst[mins[elem_ind - 1][0]])
-    return max(sums)
-
-
 n = int(input())
 lst = list(map(int, input().split()))
-mins = min_pair2()
-print(mins)
-prefix_sums = [0]
-for i in lst:
-    prefix_sums.append(prefix_sums[-1] + i)
-prefix_sums.pop(0)
-print(prefix_sums)
-mx = -1e20
-for l, r in mins:
-    cur = choose_best_sum(l, r)
-    if cur > mx:
-        mx = cur
-print(mx)
-'''
-6
-3 1 6 4 5 2
-
-'''
+stack = []
+max_sum = 0
+for current in lst:
+    if len(stack) == 0 or current > stack[-1][0]:
+        stack.append([current, current])
+    else:
+        in_while = False
+        flag = False
+        while len(stack) and current < stack[-1][0]:
+            flag = False
+            in_while = True
+            last = stack.pop()
+            current_sum = last[0] * last[-1]
+            if current_sum > max_sum:
+                max_sum = current_sum
+            if len(stack) and current < stack[-1][0]:
+                stack[-1][-1] += last[-1]
+                flag = True
+        if not flag:
+            if in_while:
+                stack.append([current, last[-1] + current])
+            else:
+                to_add = stack.pop(-1)[-1]
+                stack.append([current, to_add + current])
+                curr = stack[-1][0] * stack[-1][-1]
+                if curr > max_sum:
+                    max_sum = curr
+while len(stack):
+    current_sum = -1
+    while current_sum <= max_sum and len(stack):
+        last = stack.pop()
+        current_sum = last[0] * last[-1]
+        if len(stack):
+            stack[-1][-1] += last[-1]
+    if current_sum > max_sum:
+        max_sum = current_sum
+print(max_sum)
